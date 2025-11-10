96867a735949c11d50c0dbbc88aa8fde0b3aa0e7
Courtney Kimbrough <courtneyekimbrough@gmail.com>
2025-11-09 23:05:25 -0600

    fetch from api

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,20 +1,24 @@
 import Card from './Card'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Board(){
-    const [species, setSpecies] = useState('data')
+    const [species, setSpecies] = useState({})
 
     useEffect(() => {
         async function fetchData() {
-            const response = fetch('https://pokeapi.co/api/v2/pokemon-species/')
-            const data = (await response).json
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=10')
+            const data = (await response.json())
             setSpecies(data)
-        }
+        } 
+        fetchData()
     }, [])
     return(
         <>
         <Card></Card>
-        {species}
+        <ul>
+            {species.results ? species.results.map((item) => <li key={item.name}>{item.name}</li>) 
+            : "Loading"}
+        </ul>
         </>
     )
 }
